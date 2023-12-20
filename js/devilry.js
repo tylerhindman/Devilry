a0f4dcaab114f9fbc94700fbd66f26caea02e2d5
tylerhindman <tylerhindman64@gmail.com>
2023-12-19 23:01:39 -0600

    Added icons, closable and maximizable windows

--- js/devilry.js
+++ js/devilry.js
@@ -8,10 +8,9 @@ let resizedMouseStartX = 0;
 let resizedMouseStartY = 0;
 
 let windowsList = null;
-let zIndexCounter = 0;
+let zIndexCounter = 10;
 let currentlyFocusedWindow = null;
 
-const initWindows = 4;
 const spellRandomChatTimeMin = 5;
 const spellRandomChatTimeMax = 25;
 const spellRandomCharMin = 8;
@@ -19,12 +18,30 @@ const spellRandomCharMax = 16;
 
 function devilryStart(startEvent) {
   // ---------- Init window setup
-  for (let i = 0; i < initWindows; i++) {
+  // Initialize all windows from the start but keep them hidden (closed).
+  // They will be opened when user clicks icons.
+
+  // Following windows are chat windows (global, local, mind).
+  for (let i = 0; i < 3; i++) {
     const newWindowNode = document.getElementById('draggableWindowTemplate').content.cloneNode(true);
     const newWindowElement = newWindowNode.lastElementChild;
-    newWindowElement.id = 'draggableWindow-' + i;
+    switch (i) {
+      case 0:
+        newWindowElement.id = 'chat-window-global';
+        newWindowElement.querySelector('.draggable-window-title').textContent = 'GLOBAL_CHAT';
+        break;
+      case 1:
+        newWindowElement.id = 'chat-window-local';
+        newWindowElement.querySelector('.draggable-window-title').textContent = 'LOCAL_CHAT';
+        break;
+      case 2:
+        newWindowElement.id = 'chat-window-mind';
+        newWindowElement.querySelector('.draggable-window-title').textContent = 'MIND';
+        break;
+    }
     newWindowElement.style.top = (50 + (i * 50)) + 'px';
     newWindowElement.style.left = (50 + (i * 50)) + 'px';
+    newWindowElement.style.display = 'none';
     document.body.appendChild(newWindowNode);
   }
 
@@ -32,7 +49,6 @@ function devilryStart(startEvent) {
   let draggableWindows = document.querySelectorAll('.draggable-window');
   windowsList = draggableWindows;
   for (let i = 0; i < draggableWindows.length; i++) {
-    draggableWindows.item(i).setAttribute('minimized', 'false');
     const currWindow = draggableWindows.item(i).querySelector('.draggable-window-header');
     currWindow.addEventListener('mousedown', function (event) {
       if (draggedWindow == null && resizedAnchor == null) {
@@ -47,6 +63,11 @@ function devilryStart(startEvent) {
     document.addEventListener('mouseup', function(event) {
       currWindow.removeEventListener('mousemove', draggableWindowMouseMove);
       currWindow.removeEventListener('mousemove', resizeableAnchorMouseMove)
+      if (draggedWindow) {
+        focusWindow(draggedWindow);
+      } else if (resizedAnchor) {
+        focusWindow(resizedAnchor.parentElement.parentElement);
+      }
       draggedWindow = null;
       resizedAnchor = null;
     });
@@ -75,14 +96,24 @@ function devilryStart(startEvent) {
   // ----- Pull focused window to the top
   for (var i = 0; i < windowsList.length; i++) {
     const focusedWindow = windowsList.item(i);
-    focusedWindow.addEventListener('click', function (event) {
+    focusedWindow.addEventListener('mousedown', function (event) {
       if (currentlyFocusedWindow == null || currentlyFocusedWindow.id != focusedWindow.id) {
         focusedWindow.style.zIndex = ++zIndexCounter;
+      }
+    });
+    focusedWindow.addEventListener('click', function (event) {
+      if (currentlyFocusedWindow == null || currentlyFocusedWindow.id != focusedWindow.id) {
         focusedWindow.querySelector('.draggable-window-input').focus();
         currentlyFocusedWindow = focusedWindow;
       }
     });
   }
+}
+
+function focusWindow(focusedWindow) {
+  focusedWindow.querySelector('.draggable-window-input').focus();
+  focusedWindow.style.zIndex = ++zIndexCounter;
+  currentlyFocusedWindow = focusedWindow;
 }
 
 function draggableWindowMouseMove(event) {
@@ -110,13 +141,13 @@ function inputTextChat(event) {
       p.className = 'log-entry-parent log-entry-spell';
       windowBody.appendChild(p);
       spellDelayedAdd({spell: spells.bless}, p, windowBody);
-    }
-// -- RoomTest
-      else if (event.target.value.trim().includes('cast room')) {
-        const p = document.createElement("p");
-        p.className = 'log-entry-parent log-entry-spell';
-        windowBody.appendChild(p);
-        spellDelayedAdd({spell: rooms.castle_NESW}, p, windowBody);
+
+    // -- RoomTest
+    } else if (event.target.value.trim().includes('cast room')) {
+      const p = document.createElement("p");
+      p.className = 'log-entry-parent log-entry-spell';
+      windowBody.appendChild(p);
+      spellDelayedAdd({spell: rooms.castle_NESW}, p, windowBody);
 
     // Else, enter into chat normally
     } else {
@@ -152,7 +183,6 @@ function inputTextChat(event) {
 function spellDelayedAdd(textObj, paragraphElement, windowBody) {
   let text = textObj.spell;
   if (text.length > 0) {
-    //let randomCharAmount = Math.floor(Math.random() * (spellRandomCharMax - spellRandomCharMin));
     let randomCharAmount = text.indexOf("<br>") != -1 ? text.indexOf("<br>") + 4 : text.length;
     randomCharAmount = randomCharAmount <= text.length - 1 ? randomCharAmount : text.length; 
     paragraphElement.innerHTML += text.substring(0, randomCharAmount);
@@ -167,18 +197,105 @@ function spellDelayedAdd(textObj, paragraphElement, windowBody) {
 function minimizeClicked(event) {
   const parentWindow = event.target.parentElement.parentElement.parentElement.parentElement;
   if (parentWindow.getAttribute('minimized') == 'false') {
-    parentWindow.setAttribute('minimized', parentWindow.style.height);
-    parentWindow.querySelector('.draggable-window-body').style.display = 'none';
-    parentWindow.querySelector('.draggable-window-footer').style.display = 'none';
-    parentWindow.querySelector('.resize-anchor').style.display = 'none';
-    parentWindow.querySelector('.draggable-window-background').style.display = 'none';
-    parentWindow.style.height = '25px';
+    // Restore from maximize if maximized
+    if (parentWindow.getAttribute('maximized') != 'false') {
+      maximizeWindow(parentWindow, 'restore');
+    }
+    minimizeWindow(parentWindow, 'minimize');
   } else {
-    parentWindow.querySelector('.draggable-window-body').style.display = 'block';
-    parentWindow.querySelector('.draggable-window-footer').style.display = 'block';
-    parentWindow.querySelector('.resize-anchor').style.display = 'block';
-    parentWindow.querySelector('.draggable-window-background').style.display = 'block';
-    parentWindow.style.height = parentWindow.getAttribute('minimized');
-    parentWindow.setAttribute('minimized', 'false');
+    minimizeWindow(parentWindow, 'restore');
+    // Focus on restore
+    focusWindow(parentWindow);
+  }
+}
+
+function minimizeWindow(minWindow, minOrRestore) {
+  if (minOrRestore == 'minimize') {
+    minWindow.setAttribute('minimized', minWindow.style.height);
+    minWindow.querySelector('.draggable-window-body').style.display = 'none';
+    minWindow.querySelector('.draggable-window-footer').style.display = 'none';
+    minWindow.querySelector('.resize-anchor').style.display = 'none';
+    minWindow.querySelector('.draggable-window-background').style.display = 'none';
+    minWindow.style.height = '25px';
+  } else {
+    minWindow.querySelector('.draggable-window-body').style.display = 'block';
+    minWindow.querySelector('.draggable-window-footer').style.display = 'block';
+    minWindow.querySelector('.resize-anchor').style.display = 'block';
+    minWindow.querySelector('.draggable-window-background').style.display = 'block';
+    minWindow.style.height = minWindow.getAttribute('minimized');
+    minWindow.setAttribute('minimized', 'false');
+  }
+}
+
+function maximizeClicked(event) {
+  const parentWindow = event.target.parentElement.parentElement.parentElement.parentElement;
+  if (parentWindow.getAttribute('maximized') == 'false') {
+    // Restore from minimize if minimized
+    if (parentWindow.getAttribute('minimized') != 'false') {
+      minimizeWindow(parentWindow, 'restore');
+    }
+    maximizeWindow(parentWindow, 'maximize');
+  } else {
+    maximizeWindow(parentWindow, 'restore');
+  }
+
+  // Focus on maximize or restore
+  focusWindow(parentWindow);
+}
+
+function maximizeWindow(maxWindow, maxOrRestore) {
+  if (maxOrRestore == 'maximize') {
+    maxWindow.setAttribute('maximized', maxWindow.style.top + ':' + maxWindow.style.left + ':' + maxWindow.style.width + ':' + maxWindow.style.height);
+    maxWindow.style.top = '0px';
+    maxWindow.style.left = '0px';
+    maxWindow.style.width = '100%';
+    maxWindow.style.height = '100%';
+    maxWindow.querySelector('.resize-anchor').style.display = 'none';
+  } else {
+    const restoredProps = maxWindow.getAttribute('maximized').split(':');
+    maxWindow.style.top = restoredProps[0];
+    maxWindow.style.left = restoredProps[1];
+    maxWindow.style.width = restoredProps[2];
+    maxWindow.style.height = restoredProps[3];
+    maxWindow.querySelector('.resize-anchor').style.display = 'block';
+    maxWindow.setAttribute('maximized', 'false');
+  }
+}
+
+function closeClicked(event) {
+  const parentWindow = event.target.parentElement.parentElement.parentElement.parentElement;
+  if (parentWindow != null && parentWindow.getAttribute('closed') == 'false') {
+    parentWindow.style.display = 'none';
+    parentWindow.setAttribute('closed', 'true');
+  }
+}
+
+function iconClicked(event, iconName) {
+  // Get window reference based on supplied iconName
+  let selectedWindow = null;
+  switch(iconName) {
+    case 'globalChat':
+      selectedWindow = document.getElementById('chat-window-global');
+      break;
+    case 'localChat':
+      selectedWindow = document.getElementById('chat-window-local');
+      break;
+    case 'mind':
+      selectedWindow = document.getElementById('chat-window-mind');
+      break;
+  }
+
+  // Only open the window if it is already closed
+  if (selectedWindow != null && selectedWindow.getAttribute('closed') == 'true') {
+    selectedWindow.style.display = 'block';
+    selectedWindow.setAttribute('closed', 'false');
+    selectedWindow.querySelector('.draggable-window-input').focus();
+    selectedWindow.style.zIndex = ++zIndexCounter;
+    currentlyFocusedWindow = selectedWindow;
+    if (selectedWindow.getAttribute('minimized') !== 'false') {
+      minimizeWindow(selectedWindow, 'restore');
+    } else if (selectedWindow.getAttribute('maximized') !== 'false') {
+      maximizeWindow(selectedWindow, 'restore');
+    }
   }
 }
